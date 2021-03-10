4f1be439a4f691bc22f042c2d4db57f386364e1b
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-10 14:28:13 -0800

    Add prompt for number guess

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 'use strict'
 
-let username = prompt('What\'s your name?');
-alert('Welcome to my site, '+ username);
+let numberGuess = parseInt(prompt('What number am I thinking of?'));
 
+// function isYesorNo(response) {
+//     response = response.toLowerCase();
 
-function isYesorNo(response) {
-    response = response.toLowerCase();
+//     if(response === 'yes' || response === 'no') {
+//         // console.log('success, '+ response);
+//         return(response);
+//     } 
+//     else {
+//         // console.log('fail, '+ response); 
+//     } 
+// }
 
-    if(response === 'yes' || response === 'no') {
-        // console.log('success, '+ response);
-        return(response);
-    } 
-    else {
-        // console.log('fail, '+ response); 
-    } 
-}
+// function checkAnswer(response, right, wrong) {
+//     response = isYesorNo(response);
 
-function checkAnswer(response, right, wrong) {
-    response = isYesorNo(response);
+//     if(response === 'yes') {
+//         alert(right);
+//     } else {
+//         alert(wrong);
+//     }
+// }
 
-    if(response === 'yes') {
-        alert(right);
-    } else {
-        alert(wrong);
-    }
-}
-
-checkAnswer(prompt('Am I young?'), 'Yes, I\'m young', 'I\'m not old.');
-checkAnswer(prompt('Am I short?'), 'Correct, I\'m only 5\'8"', 'I\'m not tall.');
-checkAnswer(prompt('Do I own a dog?'), 'Yes, I do own a dog.', 'Wrong, I do own a dog');
-checkAnswer(prompt('Is my dog\'s name Doug?'), 'Yes, his name is Doug', 'Think again.');
-checkAnswer(prompt('Do I live in a house?'), 'Yes, I live in a house', 'Wrong, I do live in a house');
+// let username = prompt('What\'s your name?');
+// alert('Welcome to my site, '+ username);
+// checkAnswer(prompt('Am I young?'), 'Yes, I\'m young', 'I\'m not old.');
+// checkAnswer(prompt('Am I short?'), 'Correct, I\'m only 5\'8"', 'I\'m not tall.');
+// checkAnswer(prompt('Do I own a dog?'), 'Yes, I do own a dog.', 'Wrong, I do own a dog');
+// checkAnswer(prompt('Is my dog\'s name Doug?'), 'Yes, his name is Doug', 'Think again.');
+// checkAnswer(prompt('Do I live in a house?'), 'Yes, I live in a house', 'Wrong, I do live in a house');
 
 alert('See you around, '+ username);
