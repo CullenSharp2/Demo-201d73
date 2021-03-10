3a95bb6aba975de1534c9ac6f7af566533b78497
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-10 10:30:08 -0800

    Test new commit template
    Just testing to see if the new template works.

--- js/demoSwitch.js
+++ js/demoSwitch.js
@@ -3,14 +3,18 @@
 //switch to find my favorite color
 switch (prompt('what\'s my favorite color').toLowerCase()) {
     case 'orange' :
-        console.log('Orange is my favorite!');
+        // console.log('Orange is my favorite!');
+        alert('Orange is my favorite!');
         break;
     case 'yellow' :
-        console.log('Yellow is my favorite!');
+        // console.log('Yellow is my favorite!');
+        alert('Yellow is my favorite!');
         break;
     case 'teal' :
-        console.log('Teal is my favorite!');
+        // console.log('Teal is my favorite!');
+        alert('Teal is my favorite!');
         break;
     default:
-        console.log('Sorry, you guessed wrong...')
+        // console.log('Sorry, you guessed wrong...');
+        alert('Sorry, you guessed wrong...');
 }
