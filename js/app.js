c3be6553ed8106ad5f6f67b960a749cd555ca61e
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-10 16:55:22 -0800

    Implement score variable

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 'use strict'
-// let guesses = 4;
+
+let guesses = 4;
 let guesses2 = 6;
 let answers = [6,10,20];
 let isCorrect = false;
+let score = 0;
 
 while(guesses2 > 0 && isCorrect === false) {
     let guess = parseInt(prompt('What number am I thinking of between 0 and 20?'));
 
-    for(let i = 0; i < answers.length; i ++) {
+    for(let i = 0; i < answers.length; i++) {
         if(guess === answers[i]) {
             alert('You got it. Possible answers were 6, 10, and 20.');
             isCorrect = true;
+            score++;
             break;
         } 
     }
@@ -21,50 +24,56 @@ while(guesses2 > 0 && isCorrect === false) {
     guesses2--;
 }
 
-// while(guesses > 0) {
-//     let guess = parseInt(prompt('What number am I thinking of?'));
+console.log(score);
 
-//     if(guess !== 3 && guesses === 1) {
-//         alert('Sorry, you\'re out of guesses. The number was 3.');
-//         guesses--;
-//     } else if (guess === 3) {
-//         alert('Congratulations you guessed right. The number was 3.');
-//         break;
-//     } else if (guess > 3) {
-//         alert('Your guess was too high.');
-//         guesses--;
-//     } else if (guess < 3) {
-//         alert('Your guess was too low.');
-//         guesses--;
-//     }
-// }
+while(guesses > 0) {
+    let guess = parseInt(prompt('What number am I thinking of?'));
 
-// function isYesorNo(response) {
-//     response = response.toLowerCase();
+    if(guess !== 3 && guesses === 1) {
+        alert('Sorry, you\'re out of guesses. The number was 3.');
+        guesses--;
+    } else if (guess === 3) {
+        alert('Congratulations you guessed right. The number was 3.');
+        score++;
+        break;
+    } else if (guess > 3) {
+        alert('Your guess was too high.');
+        guesses--;
+    } else if (guess < 3) {
+        alert('Your guess was too low.');
+        guesses--;
+    }
+}
 
-//     if(response === 'yes' || response === 'no') {
-//         return(response);
-//     } 
-//     else {
-//     } 
-// }
+console.log(score);
 
-// function checkAnswer(response, right, wrong) {
-//     response = isYesorNo(response);
+function isYesorNo(response) {
+    response = response.toLowerCase();
 
-//     if(response === 'yes') {
-//         alert(right);
-//     } else {
-//         alert(wrong);
-//     }
-// }
+    if(response === 'yes' || response === 'no') {
+        return(response);
+    } 
+    else {
+    } 
+}
 
-// let username = prompt('What\'s your name?');
-// alert('Welcome to my site, '+ username);
-// checkAnswer(prompt('Am I young?'), 'Yes, I\'m young', 'I\'m not old.');
-// checkAnswer(prompt('Am I short?'), 'Correct, I\'m only 5\'8"', 'I\'m not tall.');
-// checkAnswer(prompt('Do I own a dog?'), 'Yes, I do own a dog.', 'Wrong, I do own a dog');
-// checkAnswer(prompt('Is my dog\'s name Doug?'), 'Yes, his name is Doug', 'Think again.');
-// checkAnswer(prompt('Do I live in a house?'), 'Yes, I live in a house', 'Wrong, I do live in a house');
+function checkAnswer(response, right, wrong) {
+    response = isYesorNo(response);
 
-alert('See you around, '+ username);
+    if(response === 'yes') {
+        alert(right);
+        score++;
+    } else {
+        alert(wrong);
+    }
+}
+
+let username = prompt('What\'s your name?');
+alert('Welcome to my site, '+ username);
+checkAnswer(prompt('Am I young?'), 'Yes, I\'m young', 'I\'m not old.');
+checkAnswer(prompt('Am I short?'), 'Correct, I\'m only 5\'8"', 'I\'m not tall.');
+checkAnswer(prompt('Do I own a dog?'), 'Yes, I do own a dog.', 'Wrong, I do own a dog');
+checkAnswer(prompt('Is my dog\'s name Doug?'), 'Yes, his name is Doug', 'Think again.');
+checkAnswer(prompt('Do I live in a house?'), 'Yes, I live in a house', 'Wrong, I do live in a house');
+
+alert('See you around, '+ username +'. you scored ' + score + 'out of 7');
